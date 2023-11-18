9c99fe80d3882ff86f642c40084302c8e5e49037
allnc <meibrohimov@gmail.com>
2023-11-19 01:22:27 +0500

    add Try Except file

---
+++ RandomFile.py
@@ -0,0 +1,18 @@
+import random
+
+#
+# x = "WELCOME"
+#
+# print(random.choice(x))
+
+
+# mylist = ["apple", "banana", "cherry"]
+#
+# print(random.choice(mylist))
+#
+"""
+ randint(start, stop)
+"""
+# x = random.randint(3, 9)
+#
+# print(x)
