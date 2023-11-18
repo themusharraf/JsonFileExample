d926bc043c25cc1992e3b3de72a397aaf764965b
allnc <meibrohimov@gmail.com>
2023-11-19 01:01:41 +0500

    add Try Except file

---
+++ tryExcept.py
@@ -0,0 +1,11 @@
+
+"""
+1.--> try Dastur kodida kod blokini xatolar uchun sinab ko'rish imkonini beradi.
+2.--> except Dastur kodida sizga xatoni hal qilishga imkon beradi.
+3.--> else Dastur kodida xato bo'lmaganda kodni bajarishga imkon beradi.
+4.--> finally Dastur kodida sinash natijasidan qat'i nazar, kodni bajarishga imkon beradi- va bloklardan tashqari.
+"""
+try:
+  print(x)
+except:
+  print("Bu yerda x mavjud bo'lmasa")
