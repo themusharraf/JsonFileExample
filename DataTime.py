aedc5fdd6b0e9863e2bc85904a51de33bd9b1135
allnc <meibrohimov@gmail.com>
2023-11-19 01:31:43 +0500

    add RandomFile.py file

---
+++ DataTime.py
@@ -0,0 +1,11 @@
+"""
+Python dasturlash tilida sanani ifodalash uchun joriy sanani aniqlash va
+sanalar ustida amallar bajarish uchun tayyor modul bo'lib uning nomi datetime.
+
+"""
+import datetime
+import datetime
+
+"""hozir vaqt ni olish """
+x = datetime.datetime.now()
+print(x)
